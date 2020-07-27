5e4e9d582fabd6376851a094b6f588e9fc3504b2
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.com>
2020-07-27 12:14:14 +0200

    [REF] Rename OS_FileSystem_STORAGE_MAX
    OS_FileSystem_USE_STORAGE_MAX better describes the purpose of this variable,
    as it is used when max possibles size of the storage is selected for the files
    system.

--- components/Tests/src/test_OS_Keystore.c
+++ components/Tests/src/test_OS_Keystore.c
@@ -26,7 +26,7 @@ static OS_Crypto_Config_t cfgCrypto =
 static OS_FileSystem_Config_t cfgFs =
 {
     .type = OS_FileSystem_Type_FATFS,
-    .size = OS_FileSystem_STORAGE_MAX,
+    .size = OS_FileSystem_USE_STORAGE_MAX,
     .storage = IF_OS_STORAGE_ASSIGN(
         storage_rpc,
         storage_dp),
