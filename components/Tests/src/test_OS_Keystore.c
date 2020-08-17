638368f010d766c2f490413a871d6cb07ca44a7e
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-08-17 16:55:41 +0200

    [DEV] Adapt to alignment of if_OS_Entropy

--- components/Tests/src/test_OS_Keystore.c
+++ components/Tests/src/test_OS_Keystore.c
@@ -19,7 +19,7 @@
 static OS_Crypto_Config_t cfgCrypto =
 {
     .mode = OS_Crypto_MODE_LIBRARY_ONLY,
-    .library.entropy = OS_CRYPTO_ASSIGN_Entropy(
+    .entropy = IF_OS_ENTROPY_ASSIGN(
         entropy_rpc,
         entropy_port),
 };
