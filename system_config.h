edecc972d5791c2b7a9da9d0aacc76bac141f4fe
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-02-02 14:01:42 +0100

    [FIX] Remove unused Logs_Config.h
    File is not used anymore so can be removed.
    
    Fixes:
    SEOS-2087

--- system_config.h
+++ system_config.h
@@ -33,11 +33,3 @@
 // RamDisk
 //-----------------------------------------------------------------------------
 #define RAMDISK_SIZE_BYTES  (1 * 1024 * 1024)
-
-
-//-----------------------------------------------------------------------------
-// Logs
-//-----------------------------------------------------------------------------
-#define Logs_Config_LOG_STRING_SIZE         128
-#define Logs_Config_INCLUDE_LEVEL_IN_MSG    1
-#define Logs_Config_SYSLOG_LEVEL            Log_TRACE
